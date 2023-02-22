61bd41e7a42ec32a305179f1789fa56e9ccd3845
yamlakkassahun <yamlak.k@gmail.com>
2023-02-22 15:34:03 +0300

    implimented the global error handler

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+import { GlobalErrorHandler } from "./middleware/ErrorMiddelware"
+
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello Zion!'))
+app.use(GlobalErrorHandler)
 app.listen(port, () => console.log(`Zion ${process.env.NODE_ENV.toUpperCase()} Server listening on port ${port}!`))
